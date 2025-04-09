b9c2f2a6b6ee859ce1fac53286c989ffdbcff8fb
lucasbarber78 <68709724+lucasbarber78@users.noreply.github.com>
2025-04-09 08:52:45 -0400

    Update utils/__init__.py to include new anti-bot modules

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,14 +1,14 @@
 """
-Utility modules for the Restaurant Review Scraper.
+Utility modules for the restaurant review scraper.
+
+This package contains various utility functions and classes used by the scraper.
 """
 
-from src.utils.browser_utils import create_browser_session, close_browser_session
-from src.utils.date_utils import parse_date, format_date, is_date_in_range
+# Import utility modules so they can be accessed directly from src.utils
+from src.utils.date_utils import parse_date, standardize_date_format
+from src.utils.date_range_utils import get_smart_date_range, prompt_for_date_range
 
-__all__ = [
-    'create_browser_session',
-    'close_browser_session',
-    'parse_date',
-    'format_date',
-    'is_date_in_range'
-]
+# Import anti-bot detection utilities
+from src.utils.delay_utils import get_random_delay, delay_between_actions, simulate_human_typing
+from src.utils.proxy_rotation import ProxyRotator, get_browserbase_api_key
+from src.utils.stealth_plugins import StealthEnhancer, apply_stealth_measures
