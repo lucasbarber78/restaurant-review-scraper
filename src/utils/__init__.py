8ceb846976214d4ff7c745e99df189c6c9cef6a2
lucasbarber78 <68709724+lucasbarber78@users.noreply.github.com>
2025-04-09 14:49:46 -0400

    Update utils/__init__.py to import all necessary utility modules

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,9 +4,12 @@ Utility modules for the restaurant review scraper.
 This package contains various utility functions and classes used by the scraper.
 """
 
-# Import utility modules so they can be accessed directly from src.utils
+# Import basic utility modules
 from src.utils.date_utils import parse_date, standardize_date_format
 from src.utils.date_range_utils import get_smart_date_range, prompt_for_date_range
+
+# Import browser utility modules
+from src.utils.browser_utils import create_browser_session, close_browser_session, take_screenshot, save_html
 
 # Import anti-bot detection utilities
 from src.utils.delay_utils import get_random_delay, delay_between_actions, simulate_human_typing
